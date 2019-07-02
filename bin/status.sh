b2f0e346924e76e6fc76acbaf37a1aa3077d526e
Marius Nygard <marnyg31.10@gmail.com>
2019-07-02 17:39:12 +0200

    fix status

--- bin/status.sh
+++ bin/status.sh
@@ -5,26 +5,12 @@
 
 while true; do
 
-	kjemi=13
-	fys=21
-	datakom=1
-	matte=5
-	cpp=6
 	
-	NowDay=10#`date +"%d"`
-	NowM=$(date +"%m")
-	
-
-	let kjemi=$kjemi-$NowDay
-	let fys=$fys-$NowDay
-	let datakom=31+$datakom-$NowDay
-	let matte=31+$matte-$NowDay
-	let cpp=32+$cpp-$NowDay
 
 	Date=$(date +"%A %d-%m-%y %H:%M")
-	Volum=$(amixer get Master | awk -F'[][]' 'END{ print $2 }')
+	#Volum=$(amixer get Master | awk -F'[][]' 'END{ print $2 }')
 	Lang=$(setxkbmap -query | awk '/layout/{ print $2 }')
 	FreeMem=$(free -h | awk '(NR==2){ print $3 }')
-	xsetroot -name "k:$kjemi f:$fys d:$datakom m:$matte c:$cpp $Date   Vol:$Volum   Lang:$Lang   Memory:$FreeMem   "
+	xsetroot -name " $Date   Vol:$Volum   Lang:$Lang   Memory:$FreeMem   "
 	sleep 2
 done
